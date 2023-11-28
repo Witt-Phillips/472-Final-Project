0c1aaab12518ed8e5dcdc9d2e309c8647b0cf930
Witt Phillips <witt.phillips@yale.edu>
2023-11-28 16:40:20 -0500

    Lidar integration, obstacle mapping

--- ProjectReleaseV2/Zombie world/controllers/validate_inputs/validate_inputs.py
+++ ProjectReleaseV2/Zombie world/controllers/validate_inputs/validate_inputs.py
@@ -7,7 +7,18 @@ from youbot_zombie import *
    
 #------------------CHANGE CODE BELOW HERE ONLY--------------------------
 #define functions here for making decisions and using sensor inputs
-    
+
+
+#Addditional packages/ constants---------------------------------------------------------------
+# import matplotlib.pyplot as plt
+# from matplotlib.animation import FuncAnimation
+# import numpy as npy
+from controller import Display
+import math
+_2PI = 2 * math.pi
+#--------------------------------------------------------------------------------
+
+
 # Map data structure
     # Uses a hash map with GPS-derived coords as keys.
     # Values are objects in MapCell, which hold relevant cell info.
@@ -16,21 +27,63 @@ class Map:
          self.cellWidth = cellWidth #in meters
          self.cellTable = {}
          self.currCell = currCell
+         self.initialReading = []
         
-    def gps_to_map(self, initialReading, gpsVal):
-        dist_from_origin = gpsVal - initialReading
+    def gps_to_map(self, initial, gpsVal):
+        dist_from_origin = gpsVal - initial
         index = dist_from_origin // self.cellWidth
         return int(index)
+        
+    def map_lidar(self, beamNum, dist, ori):
+        #Normalize angle
+        angle = ((_2PI / 512) * beamNum) + ori
+        if (angle > _2PI):
+            angle = angle - _2PI
+        #print("Beam", beamNum , "read distance", round(dist, 3), "at angle", math.degrees(angle))
+        
+        #Find coords & map
+        xPos = math.cos(angle) * dist
+        yPos = math.sin(angle) * dist
+        xMapped = self.gps_to_map(self.initialReading[0], xPos)
+        yMapped = self.gps_to_map(self.initialReading[1], yPos)
+        coords = str_coords(xMapped, yMapped)
+        #print("Coords are", coords)
+
+        #print("Object detected at (", xPos, ",", yPos, ")")
+        
+        if self.cellTable.get(coords) is None:
+            #Toggle 'solid' field
+            self.cellTable[coords] = MapCell(xMapped, yMapped, None, None, None, True)
+            print("New solid detected at", coords, "marked solid")
+        else:
+            cell = self.cellTable.get(coords)
+            cell.solid = True
+            print("Existing cell at", coords, "marked solid")
 
 class MapCell:
-    def __init__(self, xPos, yPos, berryType, zombieType, visited, obstacle):
+    def __init__(self, xPos, yPos, berryType, zombieType, visited, solid):
         self.xPos = xPos
         self.yPos = yPos
         self.coordStr = "(" + str(xPos) + ", " + str(yPos) + ")"
         self.berryType = berryType
         self.zombieType = zombieType
         self.visited = visited
-        self.obstacle = obstacle
+        self.solid = solid
+
+#Note - this code is translated from the C version available in the documentation.  
+def get_comp_angle(compassVals):
+    angle = math.atan2(compassVals[1], compassVals[0]) # got rid of angle_rad - 1.5708
+    if (angle < 0.0):
+        angle = angle + _2PI
+    return angle
+
+def str_coords(x, y):
+    return "(" + str(x) + ", " + str(y) + ")"
+    
+# #Testing curr_pos visualization
+# plt.ion()
+# all_curr_pos_x = []
+# all_curr_pos_y = []
 
 #------------------CHANGE CODE ABOVE HERE ONLY--------------------------
 
@@ -127,12 +180,10 @@ def main():
     #Establish relative center from GPS
     initialX = round(gps.getValues()[0], 3)
     initialY = round(gps.getValues()[2], 3)
-   
-    mappedX = mainMap.gps_to_map(initialX, initialX)
-    mappedY = mainMap.gps_to_map(initialY, initialY)
-    
-    init_cell = MapCell(mappedX, mappedY, None, None, None, None)
-    #Verify we are centered at (0, 0)
+    mainMap.initialReading = [initialX, initialY]
+    print("Initial unmapped coords:", mainMap.initialReading)
+
+    init_cell = MapCell(0, 0, None, None, None, None)
     print("Init Cell has coords:", init_cell.coordStr)
     
    
@@ -146,40 +197,62 @@ def main():
         #Get sensor data
         gps_values = gps.getValues()
         lidar_values = lidar.getRangeImage()
-        orientation = compass.getValues()
+        compass_values = compass.getValues()
+        
+        orientation = get_comp_angle(compass_values)
         gpsX = round(gps_values[0], 3)
         gpsY = round(gps_values[2], 3)
         
         #Cast gps readings to map coords
-        mappedX = mainMap.gps_to_map(initialX, gpsX)
-        mappedY = mainMap.gps_to_map(initialY, gpsY)
-        coords = "(" + str(mappedX) + ", " + str(mappedY) + ")"
+        mappedX = mainMap.gps_to_map(mainMap.initialReading[0], gpsX)
+        mappedY = mainMap.gps_to_map(mainMap.initialReading[1], gpsY)
+        coords = str_coords(mappedX, mappedY)
         
         #Create new dictionary entry if cell unencountered
         if mainMap.cellTable.get(coords) is None:
             mainMap.cellTable[coords] = MapCell(mappedX, mappedY, None, None, None, None)    
         
         #Manipulate current cell
+        mainMap.currCell = mainMap.cellTable.get(coords)
         curr_cell = mainMap.cellTable.get(coords)
         curr_cell.visited = True
         
+        # all_curr_pos_x.append(curr_cell.xPos)
+        # all_curr_pos_y.append(curr_cell.xPos)
         
-        #Testing
-        testingCoords = mainMap.cellTable.get(coords).coordStr
-        #print("Current cell visited?:", mainMap.cellTable.get(coords).visited)        
-        #print("GPS yields", gpsX, ",", gpsY)
-        if testingCoords != temp_coords:
-            print("Map Position:", testingCoords)
-        temp_coords = testingCoords
-        
+        #Lidar Mapping
+        for i in range(len(lidar_values)):
+            if lidar_values[i] != float('inf') and lidar_values[i] != 0.0:
+                mainMap.map_lidar(i, lidar_values[i], orientation)
+                #print(lidar_values)
+                #print("Orientation is", orientation)
+                #print("i =", i, ",lidar_values[i] =", lidar_values[i], ", ori =", orientation)
+
+       
         #Motor Control     
         fr.setVelocity(2.0)
         fl.setVelocity(4.0)
         br.setVelocity(2.0)
         bl.setVelocity(4.0)
+        
+        #Testing
+        # testingCoords = mainMap.cellTable.get(coords).coordStr
+        # #print("Current cell visited?:", mainMap.cellTable.get(coords).visited)        
+        # #print("GPS yields", gpsX, ",", gpsY)
+        # if testingCoords != temp_coords:
+            # print("Map Position:", testingCoords)
+        # temp_coords = testingCoords
+        
+        # #Plotting
+        # plt.xlim(min(all_curr_pos_x) - 5, max(all_curr_pos_x) + 5)
+        # plt.ylim(min(all_curr_pos_y) - 5, max(all_curr_pos_y) + 5)
+        # plt.scatter(all_curr_pos_x, all_curr_pos_y, color='black')
+        # plt.pause(timestep)
+        # #plt.show()
+        
     
     i=0
-           
+         
 
     #------------------CHANGE CODE ABOVE HERE ONLY--------------------------
     
@@ -231,11 +304,14 @@ def main():
         #i+=1
         
         #make decisions using inputs if you choose to do so
-         
+        
+                 
         #------------------CHANGE CODE ABOVE HERE ONLY--------------------------
         
-        
+    
     return 0   
 
 
 main()
+
+
